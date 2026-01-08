0d93ce32eccd03dcddd0411b5b717b15d4eed786
Dragoi Naomi <naomidragoi@yahoo.ro>
2026-01-08 03:36:17 +0200

    Creare clasa Student

---
+++ src/student.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+
+// Am creat clasa Student 
+class Student {
+private:
+    int id;             // identificator unic al studentului
+    std::string nume;   
+//Studentul va avea un id si un nume
+
+public:
+    Student();
+
+    Student(int id, const std::string& nume);
+
+    //Getteri pentru citire
+    int getId() const;
+    std::string getNume() const;
+
+    // Getteri pentru modificari
+    void setId(int idNou);
+    void setNume(const std::string& numeNou);
+};
