82aa1affda211f145b7c68e1957b12af1f75444e
Dragoi Naomi <naomidragoi@yahoo.ro>
2026-01-09 03:20:27 +0200

    Continuare pentru functia de adaugare a studentului si actualizare in meniu.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include "student.h"
 
 using namespace std;
 
@@ -15,22 +17,23 @@ void AfisareMeniu()
    cout <<" 5. Calculeaza media studentului " << endl;
    cout <<" 0. Iesire" << endl;
    cout <<" Alegeti o optiune " << endl;
-   //Am  creat meniul pe care il va vedea utilizatorul
+   // Am  creat meniul pe care il va vedea utilizatorul
 }
 
 int main() {
    int optiune = -1; 
+   Student s;
 
-   //Verificam daca optiunea introdusa este corecta
+   // Verificam daca optiunea introdusa este corecta
    while (true){
     AfisareMeniu();
     
-    cin >> optiune; //Se alege optiunea dorita
+    cin >> optiune; // Se alege optiunea dorita
     
     if (cin.fail()){
         cout<<"Optiunea introdusa este invalida. Incercati din nou." << endl;
 
-        //Curatam eroarea si stergem ce a ramas in buffer
+        // Curatam eroarea si stergem ce a ramas in buffer
         cin.clear();
         cin.ignore(1000, '\n');
 
@@ -46,9 +49,25 @@ int main() {
    }
    switch (optiune){
 
-    case 1:
-            cout << "[TODO] Aici se va adauga un student" << endl;
-            break;
+    case 1:{
+                
+        int id;
+        std::string nume;
+
+        cout << "Introduceti id-ul: ";
+        cin >> id;
+        cin.ignore(1000, '\n');
+
+        cout << "Introduceti numele: ";
+        getline(cin, nume);
+
+        s.setId(id);
+        s.setNume(nume);
+
+        cout << "Student adaugat: " << s.getId() << " " << s.getNume() << endl;
+        break;
+    }
+
     
     case 2:
             cout << "[TODO] Aici se va adauga o materie" << endl;
@@ -67,7 +86,7 @@ int main() {
             break;
 
     default:
-            cout << "Optiune invalida incearca din nou" << endl;
+            cout << "Optiune invalida. Incercati din nou" << endl;
             break;
    }
 
