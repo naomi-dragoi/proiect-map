5b09ac04f16ac81387f9dc0a96b3a0ad9dce5564
Dragoi Naomi <naomidragoi@yahoo.ro>
2026-01-09 03:24:07 +0200

    Completare commit anterior

---
+++ src/student.cpp
@@ -0,0 +1,27 @@
+#include "Student.h"
+
+// Constructor fara parametri
+Student::Student() : id(0), nume("") {}
+
+// Constructor cu parametri
+Student::Student(int id, const std::string& nume) : id(id), nume(nume) {}
+
+// Getter pentru id
+int Student::getId() const {
+    return id;
+}
+
+// Getter pentru nume
+std::string Student::getNume() const {
+    return nume;
+}
+
+// Setter pentru id
+void Student::setId(int idNou) {
+    id = idNou;
+}
+
+// Setter pentru nume
+void Student::setNume(const std::string& numeNou) {
+    nume = numeNou;
+}
